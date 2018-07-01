45a6c397bff92a3d82c29071a103a73a55c42855
Sean Cox <cox@seans-mac-pro.lan>
2018-07-01 12:04:01 -0400

    new card and data

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ class App extends Component {
     searchTerm: "",
     dualshockFilter: false,
     moveFilter: false,
-    aimFilter: false
+    aimFilter: false,
+    flightFilter: false,
+    wheelFilter: false,
+    bikeFilter: false,
+    headsetFilter: false
   };
 
   updateSearchTerm(term) {
@@ -22,7 +26,7 @@ class App extends Component {
   }
 
   toggleController(controller) {
-    this.setState({ [controller]: !this.state[controller] });
+    this.setState(prevState => ({ [controller]: !prevState[controller] }));
   }
 
   filterList() {
@@ -33,6 +37,14 @@ class App extends Component {
       gamelist = gamelist.filter(game => game.controlTypes.move);
     if (this.state.aimFilter)
       gamelist = gamelist.filter(game => game.controlTypes.aim);
+    if (this.state.flightFilter)
+      gamelist = gamelist.filter(game => game.controlTypes.flightstick);
+    if (this.state.wheelFilter)
+      gamelist = gamelist.filter(game => game.controlTypes.wheel);
+    if (this.state.bikeFilter)
+      gamelist = gamelist.filter(game => game.controlTypes.virzoom);
+    if (this.state.headsetFilter)
+      gamelist = gamelist.filter(game => game.controlTypes.psvr);
     return gamelist
       .filter(
         game =>
@@ -45,7 +57,7 @@ class App extends Component {
           game.genre.toLowerCase().includes(this.state.searchTerm.toLowerCase())
       )
       .map(gameInfo => (
-        <Col key={gameInfo.title} xs="12" md="6">
+        <Col key={gameInfo.title} xs="12" md="4">
           <GameCard {...gameInfo} />
         </Col>
       ));
@@ -57,15 +69,16 @@ class App extends Component {
         <NavBar />
         <Container style={{ marginTop: "100px" }}>
           <Row>
-            <Col xs="12" md="4">
+            <Col xs="12" md="4" style={{ marginBottom: "8px" }}>
               <Input
                 value={this.state.searchTerm}
                 placeholder="Search"
                 onChange={e => this.updateSearchTerm(e.target.value)}
               />
             </Col>
-            <Col xs={{ size: 3, offset: 3 }} md={{ size: 1, offset: 5 }}>
+            <Col xs={{ size: 3, offset: 0 }} md={{ size: 1, offset: 0 }}>
               <Button
+                style={{ marginBottom: "8px" }}
                 color={this.state.dualshockFilter ? "warning" : "primary"}
                 onClick={() => this.toggleController("dualshockFilter")}
               >
@@ -86,6 +99,38 @@ class App extends Component {
                 onClick={() => this.toggleController("aimFilter")}
               >
                 Aim
+              </Button>
+            </Col>
+            <Col xs="3" md="1">
+              <Button
+                color={this.state.flightFilter ? "warning" : "primary"}
+                onClick={() => this.toggleController("flightFilter")}
+              >
+                Flight
+              </Button>
+            </Col>
+            <Col xs="3" md="1">
+              <Button
+                color={this.state.wheelFilter ? "warning" : "primary"}
+                onClick={() => this.toggleController("wheelFilter")}
+              >
+                Wheel
+              </Button>
+            </Col>
+            <Col xs="3" md="1">
+              <Button
+                color={this.state.bikeFilter ? "warning" : "primary"}
+                onClick={() => this.toggleController("bikeFilter")}
+              >
+                Bike
+              </Button>
+            </Col>
+            <Col xs="3" md="2">
+              <Button
+                color={this.state.headsetFilter ? "warning" : "primary"}
+                onClick={() => this.toggleController("headsetFilter")}
+              >
+                Headset
               </Button>
             </Col>
           </Row>
